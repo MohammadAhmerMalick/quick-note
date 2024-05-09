215ae20e0dd63fa5ae62e35f40117927711a5d74
unknown <mohammadahmermalick@gmail.com>
2024-05-10 00:19:01 +0400

    feat: file upload feature added

--- app/api/note/route.ts
+++ app/api/note/route.ts
@@ -1,6 +1,9 @@
+import path from 'path'
 import { randomUUID } from 'crypto'
+import { writeFile } from 'fs/promises'
 import { getApps, initializeApp, cert } from 'firebase-admin/app'
 import { Timestamp, getFirestore } from 'firebase-admin/firestore'
+import { getDownloadURL, getStorage } from 'firebase-admin/storage'
 
 import serviceAccount from '@/config/firebaseConfig.json'
 
@@ -8,30 +11,54 @@ const firebaseApp = getApps()
 
 // prevents the firebase to initialize twice
 if (firebaseApp.length === 0)
-  initializeApp({ credential: cert(serviceAccount) })
+  initializeApp({
+    credential: cert(serviceAccount),
+    storageBucket: `gs://${serviceAccount.projectId}.appspot.com`,
+  })
 
 // ////////////
-// Post Request
+// POST REQUEST
 export const POST = async (request: Request) => {
   // get data from the request
   const formData = await request.formData()
   const title = formData.get('title')
+  const file = formData.get('file') as File
   const description = formData.get('description')
 
-  const db = getFirestore()
+  // set unique name for file to upload to prevent file name conflict
+  const destinationFileName = `${randomUUID()}_${file.name}`
 
   // new note data
   const newNote = {
     title,
     description,
     files: [
-      // { url: '/', name: 'file name' }
+      {
+        size: file.size,
+        type: file.type,
+        name: destinationFileName,
+      },
     ],
     cretedAt: Timestamp.now(),
     deletedAt: null,
   }
 
-  // push the new not to firebase
+  const fileLocalPath = path.join(
+    process.cwd(),
+    `public/assets/${destinationFileName}`
+  )
+
+  // upload file to local
+  const buffer = Buffer.from(await file.arrayBuffer())
+  await writeFile(fileLocalPath, buffer)
+
+  // upload file to firebase bucket
+  const uploadedFile = await getStorage()
+    .bucket()
+    .upload(fileLocalPath, { destination: destinationFileName })
+
+  // push the new note to firebase
+  const db = getFirestore()
   const res = await db.collection('note').doc(randomUUID()).set(newNote)
 
   // Response on success
