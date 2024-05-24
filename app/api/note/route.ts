9cec4f4fb8dc141f2d1050cb8d6e20abe7865f6f
unknown <mohammadahmermalick@gmail.com>
2024-05-24 20:44:44 +0400

    fix: firebase admin app initialization moved to base layout to prevent app initialization error

--- app/api/note/route.ts
+++ app/api/note/route.ts
@@ -1,17 +1,4 @@
-import { getApps, initializeApp, cert } from 'firebase-admin/app'
-import { getDownloadURL, getStorage } from 'firebase-admin/storage'
-
-import serviceAccount from '@/config/firebaseConfig.json'
 import post from './post'
-
-const firebaseApp = getApps()
-
-// prevents the firebase to initialize twice
-if (firebaseApp.length === 0)
-  initializeApp({
-    credential: cert(serviceAccount),
-    storageBucket: `gs://${serviceAccount.projectId}.appspot.com`,
-  })
 
 // ////////////
 // POST REQUEST
