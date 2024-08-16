ca71a0c99970e624ad5b9fdbafb5ec0023081a77
unknown <mohammadahmermalick@gmail.com>
2024-08-16 21:59:53 +0400

    feat: input autofocus added on create note page

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+
 import { classnames } from '@/utils'
 
 interface Input {
@@ -21,6 +23,17 @@ const Input = ({
   labelClassName,
   onChange,
 }: Input) => {
+  const TITLE_INPUT = useRef<HTMLInputElement | null>(null)
+
+  const handleChange = (e: EventTarget & HTMLInputElement) => {
+    TITLE_INPUT.current?.focus()
+    onChange(e)
+  }
+
+  useEffect(() => {
+    TITLE_INPUT.current?.focus()
+  }, [])
+
   return (
     <label
       htmlFor={id}
@@ -32,16 +45,17 @@ const Input = ({
       {labelText}
       <input
         id={id}
-        required={isRequired}
         type={type}
         value={value}
+        ref={TITLE_INPUT}
+        required={isRequired}
         placeholder={placeholder}
         className={classnames(
           labelText && 'mt-3',
           'border border-neutral-300 dark:border-neutral-600 focus:border-yellow-500 dark:focus:border-yellow-500',
           'bg-neutral-50 text-neutral-900 text-xs rounded-md focus:ring-1 focus:ring-yellow-200 block w-full p-2.5 dark:bg-neutral-850 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-1 dark:focus:ring-yellow-950 focus-visible:outline-0'
         )}
-        onChange={({ currentTarget }) => onChange(currentTarget)}
+        onChange={({ currentTarget }) => handleChange(currentTarget)}
       />
     </label>
   )
