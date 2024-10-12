44da14b865bd7c4ce5e2ac37cc763a0bb8294ff9
unknown <mohammadahmermalick@gmail.com>
2024-10-12 22:32:14 +0400

    chore: minor styling fixes

--- app/components/IconButton.tsx
+++ app/components/IconButton.tsx
@@ -30,7 +30,7 @@ const IconButton = ({
         isActive
           ? 'shadow border-yellow-500 ring-yellow-200 dark:ring-yellow-950 ring-1 bg-white dark:bg-neutral-850 z-10'
           : 'border-neutral-300 dark:border-neutral-600',
-        'text-neutral-900 md:p-2 p-2 outline-0 rounded-md border',
+        'text-neutral-900 p-2 outline-0 rounded-md border',
         'hover:bg-neutral-100 dark:hover:bg-neutral-600 focus:bg-neutral-100 dark:focus:bg-neutral-850',
         className
       )}
