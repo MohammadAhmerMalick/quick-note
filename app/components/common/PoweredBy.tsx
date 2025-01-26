66dd502f5fbf51005290d2f5689159413e87ad8c
Mohammad Ahmer Malick <mohammadahmermalick@gmail.com>
2025-01-26 23:49:21 +0400

    feat: Meta updated, Font loading optimized

--- app/components/common/PoweredBy.tsx
+++ app/components/common/PoweredBy.tsx
@@ -1,10 +1,10 @@
 const PoweredBy = () => {
   return (
-    <p className="mx-auto mt-1 max-w-xl p-2 text-center text-xs text-neutral-600 dark:text-neutral-500">
+    <p className="mx-auto mt-1 max-w-xl p-2 text-center text-xs text-neutral-600 dark:text-neutral-400">
       Powered by:{' '}
       <a
         href="https://www.mohammadahmermalick.com"
-        className="border-b border-neutral-500 text-neutral-950 dark:text-neutral-400"
+        className="border-b border-neutral-500 text-neutral-950 dark:text-neutral-200"
       >
         Mohammad Ahmer Malick
       </a>
